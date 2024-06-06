6988e85c4bdea796fb974efbdfcb4892979dca53
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-06 20:02:39 +0200

    Update

--- getfunded/my-project/src/App.jsx
+++ getfunded/my-project/src/App.jsx
@@ -3,26 +3,26 @@ import { Navbar, Hero, Stats, Business, CardDeal, Testimonials, Clients, CTA, Fo
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={'${styles.paddingX} ${styles.flexCenter}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
           <Navbar/>
         </div>
       </div>
 
-      <div className={'bg-primary ${styles.flexStart}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero/>
         </div>
       </div>
 
-      <div className={'bg-primary ${styles.flexStart}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero/>
         </div>
       </div>
       
-      <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Stats/>
           <Business/>
           <CardDeal/>
