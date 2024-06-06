db99a338b51028626483c13ac7741711c4795954
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-06 08:51:03 +0200

    From TSX to JSX

--- getfunded_V01/getfunded-project/src/App.jsx
+++ getfunded_V01/getfunded-project/src/App.jsx
@@ -1,27 +1,34 @@
-import styles from './style';
+import styles from './style.js';
+import { Navbar, Hero, Stats, Business, CardDeal, Testimonials, Clients, CTA, Footer} from './components/index.js';
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <div className={'${styles.paddingX} ${styles.flexcenter}'}>
         <div className={'${styles.boxWidth}'}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
 
       <div className={'bg-primary ${styles.flexStart}'}>
         <div className={'${styles.boxWidth}'}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       <div className={'bg-primary ${styles.flexStart}'}>
         <div className={'${styles.boxWidth}'}>
-          Hero
+          <Hero/>
         </div>
       </div>
-      <div className={'bg-primary ${styles.flexStart}'}>
+      <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
         <div className={'${styles.boxWidth}'}>
-          Hero
+          <Stats/>
+          <Business/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
     </div>
