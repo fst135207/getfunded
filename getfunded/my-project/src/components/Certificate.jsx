bd7543099c390661dacaa7f9002b036eac21df19
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-09 00:23:20 +0200

    Test update 09.06.2024

--- getfunded/my-project/src/components/Certificate.jsx
+++ getfunded/my-project/src/components/Certificate.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { cert1, cert2, cert3, cert4, cert5 } from "../assets";
 import styles, { layout } from "../style";
 import ButtonTrustpilot from "./ButtonTrustpilot";
 import ButtonDiscordserver from "./ButtonDiscordserver";
 
 const Certificate = () => {
+  const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
     return (
       <section id="certificate" className={layout.section}>
         <div className={layout.sectionInfo}>
@@ -21,14 +32,26 @@ const Certificate = () => {
                 <ButtonDiscordserver styles={`mt-10 mr-5`} />
             </div>
           </div>
-          <div className="flex flex-wrap justify-center items-center w-full">
-            <img src={cert1} alt="cert1" className="max-w-[500px] mt-20 mr-5"></img>
-            <img src={cert2} alt="cert2" className="max-w-[500px] mt-20 mr-5"></img>
-            <img src={cert3} alt="cert3" className="max-w-[500px] mt-20 mr-5"></img>
-            <img src={cert4} alt="cert4" className="max-w-[500px] mt-20 mr-5"></img>
-            <img src={cert5} alt="cert5" className="max-w-[500px] mt-20 mr-5"></img>
-          </div>
-        </div>
+          <Slider {...settings}>
+            <div>
+            <div>
+              <img src={cert1} alt="cert1" className="max-w-[500px] mt-20 mx-auto" />
+            </div>
+            <div>
+              <img src={cert2} alt="cert2" className="max-w-[500px] mt-20 mx-auto" />
+            </div>
+            <div>
+              <img src={cert3} alt="cert3" className="max-w-[500px] mt-20 mx-auto" />
+            </div>
+            <div>
+              <img src={cert4} alt="cert4" className="max-w-[500px] mt-20 mx-auto" />
+            </div>
+            <div>
+              <img src={cert5} alt="cert5" className="max-w-[500px] mt-20 mx-auto" />
+            </div>
+            </div>
+            </Slider>
+         </div>
       </section>
     );
   };
