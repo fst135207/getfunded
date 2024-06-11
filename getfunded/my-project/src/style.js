fadff3104a3ed859a3e4869c6e98a9681564999b
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-11 19:37:02 +0200

    Update 11.06.2024

--- getfunded/my-project/src/style.js
+++ getfunded/my-project/src/style.js
@@ -3,6 +3,7 @@ const styles = {
 
   heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
   paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
+  pricingToggle: "font-poppins font-semibold text-white xs:text-[30px] text-[30px] xs:leading-[30",
 
   heading2pricing: "text-4xl font-bold",
   paragraphpricing: "text-lg",
