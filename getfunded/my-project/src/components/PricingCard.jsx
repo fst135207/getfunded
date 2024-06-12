ae15c971b89ba67ec3436c09c068bb34dbf92bc2
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-12 14:30:21 +0200

    Update 12.06.2024

--- getfunded/my-project/src/components/PricingCard.jsx
+++ getfunded/my-project/src/components/PricingCard.jsx
@@ -11,13 +11,13 @@ const Pricing = () => {
     const [isYearly, setIsYearly] = useState(false);
 
   const packages = [
-    { name: "25'000 Account", monthlyPrice: 59, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "50'000 Account", monthlyPrice: 99, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "100'000 Account", monthlyPrice: 149, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "200'000 Account", monthlyPrice: 279, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "300'000 Account", monthlyPrice: 389, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "500'000 Account", monthlyPrice: 880, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
-    { name: "1'000'000 Account", monthlyPrice: 1460, yearlyPrice: 100, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", green: "/src/assets/greendot.png" },
+    { name: "25'000 Account", monthlyPrice: 59, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "50'000 Account", monthlyPrice: 99, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "100'000 Account", monthlyPrice: 149, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "200'000 Account", monthlyPrice: 279, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "300'000 Account", monthlyPrice: 389, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "500'000 Account", monthlyPrice: 880, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
+    { name: "1'000'000 Account", monthlyPrice: 1460, yearlyPrice: 100, description: "We accept: Credit Card (stripe), Crypto (USDT,BTC,ETC), and other payment methods.", green: "/src/assets/greendot.png" },
   ];
 
     return (
@@ -30,12 +30,12 @@ const Pricing = () => {
             </p>
         </div>
         <div className="mt-16">
-          <label htmlFor="toggle" className="inline-flex items-center justify-center cursor-pointer">
-            <span className={`${styles.pricingToggle} mt-5 justify-center text-center`}>HFT</span>
-            <div className="w-14 h-6 transition duration-200 bg-gray-300 ease-in-out rounded-full">
+          <label htmlFor="toggle" className="inline-flex items-center cursor-pointer">
+            <span className={`${styles.pricingToggle} mt-5 justify-center text-center`}>HFT Passing</span>
+            <div className="w-14 h-6 items-center transition duration-200 bg-gray-300 ease-in-out rounded-full mr-5 ml-5 justify-center">
                 <div className={`w-6 h-6 transition duration-200 ease-in-out rounded-full ${isYearly ? 'bg-primary ml-8' : 'bg-gray-500'}`}></div>
             </div>
-            <span className={`${styles.pricingToggle} mt-5 justify-center text-center`}>Non-HFT</span>
+            <span className={`${styles.pricingToggle} mt-5 justify-center text-center`}>Non-HFT Passing</span>
           </label>
           <input
             type="checkbox"
@@ -54,37 +54,37 @@ const Pricing = () => {
 
       className="grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-10 mt-20 md:w-11/12 mx-auto">
         {packages.map((pkg, index) => (
-          <div key={index} className="border py-10 md:px-6 px-4 rounded-lg shadow-3xl bg-white">
-            <h3 className="text-3xl  font-bold text-center text-[#010851]">{pkg.name}</h3>
-            <p className="text-tertiary text-center my-6">{pkg.description}</p>
-            <p className="mt-5 text-center text-secondary text-4xl font-bold">
+          <div key={index} className="py-10 md:px-6 px-4 rounded-lg shadow-3x1 bg-slate-900">
+            <h3 className="text-3xl  font-bold text-center text-[#ffffff]">{pkg.name}</h3>
+            <p className="text-white text-center my-6">{pkg.description}</p>
+            <p className="mt-5 text-center text-[#3fe999] text-4xl font-bold">
               {isYearly ? `$${pkg.yearlyPrice}` : `$${pkg.monthlyPrice}`}<span className="text-base text-tertiary font-medium">/{isYearly ? 'per account' : 'per account'}</span>
             </p>
-            <ul className="mt-4 space-y-2 px-4">
-              <li className="flex items-center">
+            <ul className="mt-8 space-y-2 px-2 text-white">
+              <li className="flex items-center justify-center">
                 <FcInfo className="mr-2 text-xl"/>
-                Videos of Lessons
+                <span style={{ width: "200px" }}>Challenge passed within 24H upon request during a working day</span>
               </li>
-              <li className="flex items-center">
+              <li className="flex items-center justify-center">
                 <FcInfo className="mr-2 text-xl"/>
-                Homework check
+                <span style={{ width: "200px" }}>I use Private VPS for every customer (no issues with IP)</span>
               </li>
-              <li className="flex items-center">
-              <FcInfo className="mr-2 text-xl"/>
-                Additional practical task
+              <li className="flex items-center justify-center">
+                <FcInfo className="mr-2 text-xl"/>
+                <span style={{ width: "200px" }}>24/7 Support</span>
               </li>
-              <li className="flex items-center">
-              <FcInfo className="mr-2 text-xl"/>
-                Monthly conferences 
+              <li className="flex items-center justify-center">
+                <FcInfo className="mr-2 text-xl"/>
+                <span style={{ width: "200px" }}>Risk Free Gurantee!</span>
               </li>
-              <li className="flex items-center">
-              <FcInfo className="mr-2 text-xl"/>
-                Personal advice from teachers
+              <li className="flex items-center justify-center">
+                <FcInfo className="mr-2 text-xl"/>
+                <span style={{ width: "200px" }}>Only Metatrader 4 supported!!!</span>
               </li>
             </ul>
 
             {/* button */}
-            <div className="w-full mx-auto flex items-center justify-center mt-5">
+            <div className="w-full mx-auto flex items-center justify-center mt-7">
             <Button/>
             </div>
           </div>
--- getfunded/my-project/src/components/index.js
+++ getfunded/my-project/src/components/index.js
@@ -6,7 +6,6 @@ import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
-import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import Certificate from "./Certificate";
 import CertificateSlider from "./CertificateSlider";
@@ -21,7 +20,6 @@ export {
   CTA,
   Stats,
   Footer,
-  Testimonials,
   Hero,
   Certificate,
   CertificateSlider,
