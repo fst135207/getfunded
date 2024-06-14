33e281c516686b533b691884e62def8f377eaa4b
fst135207 <felix.stalder.bsu@gmail.com>
2024-06-14 09:07:04 +0200

    Update

--- Client/my-project/src/main.jsx
+++ Client/my-project/src/main.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { MemberstackProvider } from '@memberstack/react';
-
-const config = { publicKey: 'pk_eafb622cdd8c558500c5' };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemberstackProvider config={config}>
       <App />
-    </MemberstackProvider>
   </React.StrictMode>
 );
